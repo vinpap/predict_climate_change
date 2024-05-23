ac556e375f04e2bcc415625a1c917a1c2a38c5ee
Vincent Papelard <vincent.papelard@logiroad-center.com>
2024-05-23 18:14:32 +0200

    Adding a function in the monitoring script so it does not run if less than one month has passed

--- api.py
+++ api.py
@@ -10,18 +10,22 @@ from pydantic import BaseModel
 from pmdarima.arima import ARIMA
 from sklearn.metrics import mean_squared_error
 
+
 class WeatherData(BaseModel):
     """
     Data structure that stores average monthly temperatures for a given period of time.
     """
+
     start_date: str
     temperatures: List[float]
     secret_token: str
+
 
 class ForecastDate(BaseModel):
     """
     Stores a single date.
     """
+
     date: str
     secret_token: str
 
@@ -35,21 +39,27 @@ def get_model():
     """
     model_path = "./model.pkl"
     dates_path = "./dates.pkl"
-    if os.path.exists(model_path) and os.path.isfile(model_path) and os.path.exists(dates_path) and os.path.isfile(dates_path):
+    if (
+        os.path.exists(model_path)
+        and os.path.isfile(model_path)
+        and os.path.exists(dates_path)
+        and os.path.isfile(dates_path)
+    ):
         print(f"Loading SARIMA model from {model_path}")
         with open(model_path, "rb") as model_file, open(dates_path, "rb") as dates_file:
             model = pickle.load(model_file)
             dates = pickle.load(dates_file)
             return model, dates
-    
+
     else:
         return False, None
-    
+
 
 # model_dates stores the range of dates covered by the trained model
 model, model_dates = get_model()
 app = FastAPI()
 security_token = os.environ["API_TOKEN"]
+
 
 @app.post("/", status_code=200)
 async def root(secret_token: str, response: Response):
@@ -57,8 +67,11 @@ async def root(secret_token: str, response: Response):
         return JSONResponse(
             status_code=401, content={"msg": "Your security code is not valid"}
         )
-    return {"message": "Welcome to our API.\
-             Please use one of these endpoints: /train, /test, /predict"}
+    return {
+        "message": "Welcome to our API.\
+             Please use one of these endpoints: /train, /test, /predict"
+    }
+
 
 @app.post("/train", status_code=200)
 async def train(data: WeatherData):
@@ -76,14 +89,13 @@ async def train(data: WeatherData):
     try:
         start_date = pd.to_datetime(data.start_date, format="%m/%Y")
     except ValueError:
-        return {"message": "Please provide the temperature series start date in format mm/yyyy"}
-    
+        return {
+            "message": "Please provide the temperature series start date in format mm/yyyy"
+        }
+
     # The hyperparameters below are those that provide the best performance
     # on monthly temperature series. See report for more info
-    model = ARIMA(
-        order=(1, 1, 1),
-        seasonal_order=(2, 0, 1, 12)
-                )
+    model = ARIMA(order=(1, 1, 1), seasonal_order=(2, 0, 1, 12))
     temperature = data.temperatures
     model.fit(temperature)
 
@@ -98,26 +110,29 @@ async def train(data: WeatherData):
         pickle.dump(model, model_file)
         pickle.dump(model_dates, dates_file)
 
-
     return {"message": "OK"}
+
 
 @app.post("/predict", status_code=200)
 async def predict(forecast_date: ForecastDate):
-
     if forecast_date.secret_token != security_token:
         return JSONResponse(
             status_code=401, content={"msg": "Your security code is not valid"}
         )
     if not model:
-        return {"message": "No model has been trained. Please train the model first by sending monthly temperature series to the /train endpoint"}
-    
+        return {
+            "message": "No model has been trained. Please train the model first by sending monthly temperature series to the /train endpoint"
+        }
+
     try:
         forecast_date = pd.to_datetime(forecast_date.date, format="%m/%Y")
     except ValueError:
         return {"message": "Please provide the forecast date in format mm/yyyy"}
-    
+
     if forecast_date <= model_dates[-1]:
-        return {"message": f"The date you entered is older than the last date stored by the forecast model, which is {str(model_dates[-1])}. Please enter a date after this one."}
+        return {
+            "message": f"The date you entered is older than the last date stored by the forecast model, which is {str(model_dates[-1])}. Please enter a date after this one."
+        }
 
     # Storing all the dates between the model's last known date and the requested date
     months_delta = forecast_date.to_period("M") - model_dates[-1].to_period("M")
@@ -127,13 +142,13 @@ async def predict(forecast_date: ForecastDate):
     for month in range(1, months_covered):
         months.append(model_dates[-1] + pd.DateOffset(months=month))
 
-    predicted_temperatures = list(model.predict(months_covered-1))
+    predicted_temperatures = list(model.predict(months_covered - 1))
 
     return {"dates": months, "temperatures": predicted_temperatures}
 
+
 @app.post("/test", status_code=200)
 async def test(data: WeatherData):
-
     if data.secret_token != security_token:
         return JSONResponse(
             status_code=401, content={"msg": "Your security code is not valid"}
@@ -142,20 +157,24 @@ async def test(data: WeatherData):
         start_date = pd.to_datetime(data.start_date, format="%m/%Y")
     except ValueError:
         return {"message": "Please provide the start date in format mm/yyyy"}
-    
+
     if not model:
-        return {"message": "No model has been trained. Please train the model first by sending monthly temperature series to the /train endpoint"}
-    
+        return {
+            "message": "No model has been trained. Please train the model first by sending monthly temperature series to the /train endpoint"
+        }
+
     test_temp = data.temperatures
     # Any test data older than the model's last registered date cannot be used and need to be removed
     if start_date <= model_dates[-1]:
         months_delta = model_dates[-1].to_period("M") - start_date.to_period("M")
         months_diff = months_delta.n
         if len(test_temp) <= months_diff:
-            return {"message": "The test data you sent does not include any unknown data, therefore it is impossible to test the model with it"}
-        test_temp = test_temp[months_diff+1:]
-    
+            return {
+                "message": "The test data you sent does not include any unknown data, therefore it is impossible to test the model with it"
+            }
+        test_temp = test_temp[months_diff + 1 :]
+
     pred_temp = model.predict(len(test_temp))
     rmse = math.sqrt(mean_squared_error(test_temp, pred_temp))
-    
+
     return {"RMSE": rmse}
--- monitoring.py
+++ monitoring.py
@@ -9,6 +9,7 @@ import sys
 import argparse
 import smtplib
 from email.mime.text import MIMEText
+from datetime import date
 
 import requests
 import pandas as pd
@@ -19,7 +20,10 @@ SMTP_PASSWORD = os.environ["SMTP_PASSWORD"]
 try:
     DB_PWD = os.environ["DB_PASSWORD"]
 except KeyError:
-    print("NOTE: DB_PASSWORD has not been set in the environment variables. This is only a problem if you are loading monitoring data from a PostgreSQL database")
+    print(
+        "NOTE: DB_PASSWORD has not been set in the environment variables. This is only a problem if you are loading monitoring data from a PostgreSQL database"
+    )
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -27,9 +31,27 @@ def parse_args():
         description="This script is used to monitor the temperature prediction model by making sure its performance is still viable",
     )
     parser.add_argument(
-        "--setup", "-s", action="store_true", help="Trains the model through the API for the first time and defines the performance threshold"
+        "--setup",
+        "-s",
+        action="store_true",
+        help="Trains the model through the API for the first time and defines the performance threshold",
     )
     return parser.parse_args()
+
+
+def should_run() -> bool:
+    """
+    Compares the current date with the last monitoring date stored in
+    monitoring_cfg.yml. Returns True if more than a month has passed, False
+    otherwise.
+    """
+    with open("./monitoring_cfg.yml", "r") as config_file:
+        cfg = yaml.safe_load(config_file)
+        last_running_date = cfg["LAST_RUNNING_DATE"]
+    current_date = date.today()
+    time_diff = current_date - last_running_date
+    return time_diff.days >= 30
+
 
 def load_data(config: dict):
     """
@@ -38,11 +60,12 @@ def load_data(config: dict):
     """
     if config["DATA"]["LOAD_FROM_CSV"]:
         data = load_from_csv(config["DATA"]["CSV_PATH"])
-    
+
     else:
         data = load_from_postgresql(config)
-    
+
     return data
+
 
 def load_from_csv(filepath: str) -> pd.DataFrame:
     """
@@ -54,9 +77,14 @@ def load_from_csv(filepath: str) -> pd.DataFrame:
 
     data = pd.read_csv(filepath)
     filtered_data = data[["measurement_dates", "temperatures"]]
-    filtered_data["measurement_dates"] = pd.to_datetime(filtered_data["measurement_dates"])
-    filtered_data["measurement_dates"] = filtered_data["measurement_dates"].dt.strftime('%m/%Y')
+    filtered_data["measurement_dates"] = pd.to_datetime(
+        filtered_data["measurement_dates"]
+    )
+    filtered_data["measurement_dates"] = filtered_data["measurement_dates"].dt.strftime(
+        "%m/%Y"
+    )
     return filtered_data
+
 
 def load_from_postgresql(config: dict) -> pd.DataFrame:
     """
@@ -66,9 +94,8 @@ def load_from_postgresql(config: dict) -> pd.DataFrame:
     the columns named "measurement_dates" and "temperatures".
     """
 
-    data = {"measurement_dates": [],
-                   "temperatures": []}
-    
+    data = {"measurement_dates": [], "temperatures": []}
+
     conn = psycopg2.connect(
         database=config["DATA"]["DB_NAME"],
         host=config["DATA"]["DB_URL"],
@@ -84,11 +111,12 @@ def load_from_postgresql(config: dict) -> pd.DataFrame:
     for record in results:
         data["measurement_dates"].append(record[0])
         data["temperatures"].append(record[1])
-    
+
     df = pd.DataFrame(data)
     df["measurement_dates"] = pd.to_datetime(df["measurement_dates"])
-    df["measurement_dates"] = df["measurement_dates"].dt.strftime('%m/%Y')
+    df["measurement_dates"] = df["measurement_dates"].dt.strftime("%m/%Y")
     return df
+
 
 def send_email(subject: str, body: str):
     """
@@ -102,13 +130,16 @@ def send_email(subject: str, body: str):
         smtp_server = cfg["EMAIL_SETTINGS"]["SMTP_SERVER"]
 
         msg = MIMEText(body)
-        msg['Subject'] = subject
-        msg['From'] = sender
-        msg['To'] = ', '.join(recipient)
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+        msg["Subject"] = subject
+        msg["From"] = sender
+        msg["To"] = ", ".join(recipient)
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
             smtp_server.login(sender, SMTP_PASSWORD)
-            smtp_server.sendmail("papelardvincent@gmail.com", recipient, msg.as_string())
+            smtp_server.sendmail(
+                "papelardvincent@gmail.com", recipient, msg.as_string()
+            )
             print("Message sent!")
+
 
 def split_dataset(data: pd.DataFrame) -> tuple:
     """
@@ -122,12 +153,12 @@ def split_dataset(data: pd.DataFrame) -> tuple:
     test = data.iloc[splitting_index:, :]
 
     return train, test
-    
+
 
 def setup():
     """
     Trains the model through the API and computes its root mean squared error.
-    Stores the result as a threshold for future monitoring. 
+    Stores the result as a threshold for future monitoring.
     """
     with open("./monitoring_cfg.yml", "r") as config_file:
         cfg = yaml.safe_load(config_file)
@@ -135,26 +166,34 @@ def setup():
 
         # Splitting the data in train and test datasets
         train, test = split_dataset(data)
-        
+
         # Training the temperature prediction model through the API
-        request_data = {"start_date": train["measurement_dates"].iloc[0],
-                         "temperatures": list(train["temperatures"]),
-                         "secret_token": API_TOKEN}
-        
+        request_data = {
+            "start_date": train["measurement_dates"].iloc[0],
+            "temperatures": list(train["temperatures"]),
+            "secret_token": API_TOKEN,
+        }
+
         response = requests.post(url=cfg["API_ENDPOINT"] + "train", json=request_data)
         if response.status_code < 200 or response.status_code >= 300:
-            raise RuntimeError("Unable to use the API, please make sure it is up and running at the URL specified in monitoring_cfg.yml")
+            raise RuntimeError(
+                "Unable to use the API, please make sure it is up and running at the URL specified in monitoring_cfg.yml"
+            )
 
         # Testing the model and saving 105% of the RMSE as the new performance threshold
-        test_request_data = {"start_date": test["measurement_dates"].iloc[0],
-                         "temperatures": list(test["temperatures"]),
-                         "secret_token": API_TOKEN}
+        test_request_data = {
+            "start_date": test["measurement_dates"].iloc[0],
+            "temperatures": list(test["temperatures"]),
+            "secret_token": API_TOKEN,
+        }
 
-        response = requests.post(url=cfg["API_ENDPOINT"] + "test", json=test_request_data)
+        response = requests.post(
+            url=cfg["API_ENDPOINT"] + "test", json=test_request_data
+        )
         new_threshold = 1.05 * response.json()["RMSE"]
 
         cfg["RMSE_THRESHOLD"] = new_threshold
-    
+
     with open("./monitoring_cfg.yml", "w") as config_file:
         yaml.dump(cfg, config_file)
 
@@ -168,41 +207,61 @@ def monitor():
     with open("./monitoring_cfg.yml") as config_file:
         cfg = yaml.safe_load(config_file)
         if "RMSE_THRESHOLD" not in cfg or cfg["RMSE_THRESHOLD"] == -1:
-            print("No performance threshold found in the configuration file. Please run this script with the --setup flag at least once before using it to monitor the prediction model")
+            print(
+                "No performance threshold found in the configuration file. Please run this script with the --setup flag at least once before using it to monitor the prediction model"
+            )
             sys.exit(0)
         data = load_data(cfg)
         threshold = cfg["RMSE_THRESHOLD"]
 
-        test_request_data = {"start_date": data["measurement_dates"].iloc[0],
-                         "temperatures": list(data["temperatures"]),
-                         "secret_token": API_TOKEN}
-        
-        response = requests.post(url=cfg["API_ENDPOINT"] + "test", json=test_request_data)
+        test_request_data = {
+            "start_date": data["measurement_dates"].iloc[0],
+            "temperatures": list(data["temperatures"]),
+            "secret_token": API_TOKEN,
+        }
+
+        response = requests.post(
+            url=cfg["API_ENDPOINT"] + "test", json=test_request_data
+        )
         rmse = response.json()["RMSE"]
 
         if rmse > threshold:
-            print(f"The model performance went above the root mean squared error set at {threshold}. Now retraining the model")
+            print(
+                f"The model performance went above the root mean squared error set at {threshold}. Now retraining the model"
+            )
 
             train, test = split_dataset(data)
             # Training the temperature prediction model through the API
-            request_data = {"start_date": train["measurement_dates"].iloc[0],
-                            "temperatures": list(train["temperatures"]),
-                            "secret_token": API_TOKEN}
-            
-            response = requests.post(url=cfg["API_ENDPOINT"] + "train", json=request_data)
+            request_data = {
+                "start_date": train["measurement_dates"].iloc[0],
+                "temperatures": list(train["temperatures"]),
+                "secret_token": API_TOKEN,
+            }
+
+            response = requests.post(
+                url=cfg["API_ENDPOINT"] + "train", json=request_data
+            )
             if response.status_code < 200 or response.status_code >= 300:
-                raise RuntimeError("Unable to use the API, please make sure it is up and running at the URL specified in monitoring_cfg.yml")
+                raise RuntimeError(
+                    "Unable to use the API, please make sure it is up and running at the URL specified in monitoring_cfg.yml"
+                )
 
             # Testing the model
-            test_request_data = {"start_date": test["measurement_dates"].iloc[0],
-                            "temperatures": list(test["temperatures"]),
-                            "secret_token": API_TOKEN}
+            test_request_data = {
+                "start_date": test["measurement_dates"].iloc[0],
+                "temperatures": list(test["temperatures"]),
+                "secret_token": API_TOKEN,
+            }
 
-            response = requests.post(url=cfg["API_ENDPOINT"] + "test", json=test_request_data)
+            response = requests.post(
+                url=cfg["API_ENDPOINT"] + "test", json=test_request_data
+            )
             rmse = response.json()["RMSE"]
 
             if rmse > threshold:
-                print(f"RMSE is still too high after retraining the model ({rmse} compared to a threshold set at {threshold}). Sending an email alert to {cfg['EMAIL_SETTINGS']['EMAIL_RECIPIENT']}")
+                print(
+                    f"RMSE is still too high after retraining the model ({rmse} compared to a threshold set at {threshold}). Sending an email alert to {cfg['EMAIL_SETTINGS']['EMAIL_RECIPIENT']}"
+                )
                 subject = "ATTENTION REQUIRED - your model's performance is getting bad"
                 message = f"This is an automated email, please do not reply.\nThe AI model deployed at {cfg['API_ENDPOINT']} had to be retrained because its error metric went above the specified Root Mean Square Error set at {threshold}.\nUnfortunately, retraining the model did not enable it to perform well enough.\nThe new RMSE is {rmse}, but the threshold is set at {threshold}. Further analysis is advised in order to get the model back on track."
 
@@ -210,19 +269,22 @@ def monitor():
                 print("The model's performance went back below the threshold")
                 subject = "Your model has been retrained"
                 message = f"This is an automated email, please do not reply.\nThe AI model deployed at {cfg['API_ENDPOINT']} had to be retrained because its error metric went above the specified Root Mean Square Error set at {threshold}.\nRetraining allowed the model to get a new RMSE set at {rmse}, which matches the threshold."
-            
+
             send_email(subject, message)
 
 
-
 if __name__ == "__main__":
-
     args = parse_args()
 
     if args.setup:
         setup()
     else:
+        if not should_run():
+            sys.exit(0)
         monitor()
 
-
-
+    with open("./monitoring_cfg.yml") as config_file:
+        cfg = yaml.safe_load(config_file)
+        cfg["LAST_RUNNING_DATE"] = date.today()
+    with open("./monitoring_cfg.yml", "w") as config_file:
+        yaml.dump(cfg, config_file)
